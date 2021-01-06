8a254e3dcb5a04d0ec95596432d2067eee234c83
Linus Bartsch <git@libasoft.de>
2021-01-06 22:32:02 +0100

    make setup.py code style black

--- setup.py
+++ setup.py
@@ -1,32 +1,32 @@
 from setuptools import setup, find_packages
 
-with open('README.rst', 'r', encoding='utf-8') as f:
+with open("README.rst", "r", encoding="utf-8") as f:
     readme = f.read()
 
 setup(
-    name='discord-interactions.py',
-    version='0.0.2',
-    description='A library around the Discord Interactions API',
+    name="discord-interactions.py",
+    version="0.0.2",
+    description="A library around the Discord Interactions API",
     long_description=readme,
-    long_description_content_type='text/x-rst',
-    url='https://github.com/LiBa001/discord-interactions.py',
-    author='Linus Bartsch',
-    author_email='pypi@libasoft.de',
-    license='MIT',
+    long_description_content_type="text/x-rst",
+    url="https://github.com/LiBa001/discord-interactions.py",
+    author="Linus Bartsch",
+    author_email="pypi@libasoft.de",
+    license="MIT",
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Environment :: Web Environment',
-        'Framework :: Flask',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Internet',
-        'Topic :: Software Development :: Libraries'
+        "Development Status :: 1 - Planning",
+        "Environment :: Web Environment",
+        "Framework :: Flask",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries",
     ],
-    python_requires='>=3.8',
-    install_requires=['flask', 'pynacl', 'requests'],
-    keywords='discord discord-py discord-bot wrapper',
+    python_requires=">=3.8",
+    install_requires=["flask", "pynacl", "requests"],
+    keywords="discord discord-py discord-bot wrapper",
     packages=find_packages(exclude=["examples", "tests"]),
-    data_files=None
+    data_files=None,
 )
