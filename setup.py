2358962e425e31b0b3234b5db633e1fbeffc392e
Linus Bartsch <git@libasoft.de>
2021-01-09 08:53:19 +0100

    increase patch version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as f:
 
 setup(
     name="discord-interactions.py",
-    version="0.0.5",
+    version="0.0.6",
     description="A library around the Discord Interactions API",
     long_description=readme,
     long_description_content_type="text/x-rst",
